1f8b2e441f073ecfc7a77e8ab5381d759f985046
Parvat R <vidyutparvat2@gmail.com>
2025-03-20 10:16:40 +0000

    Changes to be committed: 	new file:   kurekid/.gitignore 	new file:   kurekid/README.md 	new file:   kurekid/eslint.config.mjs 	new file:   kurekid/jsconfig.json 	new file:   kurekid/next.config.mjs 	new file:   kurekid/package-lock.json 	new file:   kurekid/package.json 	new file:   kurekid/public/file.svg 	new file:   kurekid/public/globe.svg 	new file:   kurekid/public/next.svg 	new file:   kurekid/public/vercel.svg 	new file:   kurekid/public/window.svg 	new file:   kurekid/src/app/favicon.ico 	new file:   kurekid/src/app/game/[q_number]/layout.js 	new file:   kurekid/src/app/game/[q_number]/page.js 	new file:   kurekid/src/app/game/layout.js 	new file:   kurekid/src/app/game/page.js 	new file:   kurekid/src/app/globals.css 	new file:   kurekid/src/app/layout.js 	new file:   kurekid/src/app/page.js 	new file:   kurekid/src/app/page.module.css

---
+++ kurekid/src/app/game/[q_number]/page.js
@@ -0,0 +1,114 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+
+
+const questions = {
+  '1': {
+    id: 1,
+    title: "Question 1",
+    coverImage: "cover.png",
+    coverVideo: null,
+    options: [
+      {
+        id: 1,
+        text: "Option 1",
+        img: "img.png",
+      },
+      {
+        id: 2,
+        text: "Option 2",
+        img: "img.png"
+      },
+      {
+        id: 3,
+        text: "Option 3",
+        img: "img.png"
+      },
+      {
+        id: 4,
+        text: "Option 4",
+        img: "img.png"
+      }
+    ],
+    correctOptionId: 2
+  },
+  '2': {
+    id: 2,
+    title: "Question 2",
+    coverImage: "cover.png",
+    coverVideo: null,
+    options: [
+      {
+        id: 1,
+        text: "Option 1",
+        img: "img.png",
+      },
+      {
+        id: 2,
+        text: "Option 2",
+        img: "img.png"
+      },
+      {
+        id: 3,
+        text: "Option 3",
+        img: "img.png"
+      },
+      {
+        id: 4,
+        text: "Option 4",
+        img: "img.png"
+      }
+    ],
+    correctOptionId: 2
+  }
+}
+
+function optionClicked(data) {
+  console.log("option id: " + data.id)
+}
+
+function Option({ option }) {
+  return (
+    <div onClick={optionClicked(option)}>
+      <h5>{option.id}</h5>
+      <h2>{option.text}</h2>
+    </div>
+  )
+}
+
+function Question({ question_id }) {
+  const questionData = questions[question_id];
+  if (!questionData) { return ( <h3>Question {question_id} not found!</h3> ) }
+
+  return (
+    <div>
+
+      <h1>{ questionData.title }</h1>
+
+      <div className='options'>
+
+        {
+          questionData.options.map( (option) => ( <Option key={option.id} option={option}></Option> ) )
+        }
+
+      </div>
+
+    </div>
+  )
+
+}
+
+
+export default function Page() {
+  const { q_number } = useParams();
+
+  return (
+    <>
+      <h1>Hi {q_number}</h1>
+      <Question question_id='1'></Question>
+      <hr></hr>
+      <Question question_id='2'></Question>
+    </>
+  )
+}
---
+++ kurekid/src/app/game/page.js
@@ -0,0 +1,5 @@
+export default function Page () {
+    return (
+        <h1>These are the questions.</h1>
+    )
+}
